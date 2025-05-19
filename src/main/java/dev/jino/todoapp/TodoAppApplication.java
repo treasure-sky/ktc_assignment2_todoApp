5075c07ec3ab7b67316e482b21363659f3b1ec2b
treasure-sky <ych0486666@gmail.com>
2025-05-20 01:09:16 +0900

    init: 프로젝트 생성

---
+++ src/main/java/dev/jino/todoapp/TodoAppApplication.java
@@ -0,0 +1,13 @@
+package dev.jino.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppApplication.class, args);
+    }
+
+}
